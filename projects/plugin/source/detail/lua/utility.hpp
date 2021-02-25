637b5751effaa5c681cbe4a209a13f4847928e52
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-02-25 15:24:40 +0300

    Утилиты для работы с кортежами добавлены в проект plugin.

---
+++ projects/plugin/source/detail/lua/utility.hpp
@@ -0,0 +1,72 @@
+#ifndef SOLUTION_PLUGIN_DETAIL_LUA_UTILITY_HPP
+#define SOLUTION_PLUGIN_DETAIL_LUA_UTILITY_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include <cstddef>
+#include <exception>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+namespace detail
+{
+    namespace lua
+    {
+        template < typename ... Types >
+        struct tuple_tail;
+
+        template < typename T >
+        struct tuple_tail < std::tuple < T > >
+        {
+            using type = std::tuple <> ;
+        };
+
+        template < typename T, typename ... Types >
+        struct tuple_tail < std::tuple < T, Types ... > >
+        {
+            using type = std::tuple < Types ... > ;
+        };
+
+        template < typename T >
+        using tuple_tail_t = typename tuple_tail < T > ::type;
+
+        template < std::size_t N >
+        struct Apply
+        {
+            template < typename F, typename T, typename ... Args >
+            static inline decltype(auto) apply_tuple(F && f, T && t, Args && ... args)
+            {
+                return Apply < N - 1U > ::apply_tuple(std::forward < F > (f), std::forward < T > (t),
+                    std::get < N - 1U > (std::forward < T > (t)), std::forward < Args > (args) ...);
+            }
+        };
+
+        template <>
+        struct Apply < 0U >
+        {
+            template < typename F, typename T, typename ... Args >
+            static inline decltype(auto) apply_tuple(F && f, T &&, Args && ... args)
+            {
+                return std::forward < F > (f)(std::forward < Args > (args) ...);
+            }
+        };
+
+        template < typename F, typename T >
+        inline decltype(auto) apply_tuple(F && f, T && t)
+        {
+            return Apply < std::tuple_size_v < std::decay_t < T > > > ::apply_tuple(
+                std::forward < F > (f), std::forward < T > (t));
+        }
+
+    } // namespace lua 
+
+} // namespace detail
+
+#endif // #ifndef SOLUTION_PLUGIN_DETAIL_LUA_UTILITY_HPP
