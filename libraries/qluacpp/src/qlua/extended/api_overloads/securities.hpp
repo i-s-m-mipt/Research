b6d94ecaab21fda3b7f5297b08e4631f437d178d
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-03-02 21:46:38 +0300

    Удалены все перегрузки и дополнительные функции из раздела extended.

--- libraries/qluacpp/src/qlua/extended/api_overloads/securities.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-const char* getClassSecurities(const std::string& class_name) const {
-  return api::getClassSecurities(class_name.c_str());
-}
-
-const char* getClassSecurities(const char* class_name) const {
-  return api::getClassSecurities(class_name);
-}
--- libraries/qluacpp/src/qlua/extended/api_overloads/service.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-unsigned int message(const std::string& text) const { return api::message(text.c_str()); }
-unsigned int message(const char* text) const { return api::message(text); }
