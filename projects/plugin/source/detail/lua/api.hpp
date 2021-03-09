6ab86caaa85bd8c2cd15cde6fd4313d541758ddf
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-03-09 03:23:45 +0300

    Выполнен перенос части функционала класса API в класс Source.

--- projects/plugin/source/detail/lua/api.hpp
+++ projects/plugin/source/detail/lua/api.hpp
@@ -13,123 +13,99 @@
 #include <unordered_map>
 
 #include "state.hpp"
-#include "entity.hpp"
-#include "vector.hpp"
-#include "types/boolean.hpp"
-#include "types/integer.hpp"
-#include "types/real.hpp"
-#include "types/string.hpp"
-#include "types/pointer.hpp"
-
-#include "source.hpp"
 #include "tables.hpp"
 
-namespace detail
+namespace solution
 {
-    namespace lua
+    namespace plugin
     {
-        class api_exception : public std::exception
+        namespace detail
         {
-        public:
-
-            explicit api_exception(const std::string & message) noexcept :
-                std::exception(message.c_str())
-            {}
-
-            explicit api_exception(const char * const message) noexcept :
-                std::exception(message)
-            {}
-
-            ~api_exception() noexcept = default;
-        };
-
-        class API
-        {
-        public:
-
-            using interval_t = Source::interval_t;
-
-        public:
-
-            explicit API(State state) : m_state(state)
-            {}
-
-            ~API() noexcept = default;
-
-        public:
-
-            const auto state() const noexcept
+            namespace lua
             {
-                return m_state;
-            }
-
-        public:
-
-            auto constant(const std::string & name) const
-            {
-                m_state.get_global(name);
-
-                auto result = m_state.top < interval_t > ().get();
-
-                m_state.pop();
-
-                return result;
-            }
-
-        public:
-
-            auto is_connected() const
-            {
-                return m_state.call < unsigned int > ("isConnected");
-            }
-
-            auto send_message(const std::string & message) const
-            {
-                return m_state.call < unsigned int > ("message", message);
-            }
-
-            auto create_source(const std::string & class_code, const std::string & asset_code,
-                interval_t interval) const
-            {
-                return lua::Source(m_state, class_code, asset_code, interval);
-            }
-
-            auto get_portfolio(const std::string & client_id, const std::string & client_code) const
-            {
-                return m_state.call < lua::table::Portfolio > ("getPortfolioInfo", client_id, client_code);
-            }
-
-            auto get_security_info(const std::string & class_code, const std::string & asset_code) const
-            {
-                return m_state.call < unsigned int > ("getSecurityInfo", class_code, asset_code);
-            }
-
-            auto send_transaction(const std::unordered_map < std::string, std::string > & transaction) const
-            {
-                m_state.get_global("sendTransaction");
-
-                m_state.new_table();
-
-                for (const auto & [field, value] : transaction)
+                class api_exception : public std::exception
                 {
-                    m_state.push_string(field);
-                    m_state.push_string(value);
+                public:
 
-                    m_state.set_table(-3);
-                }
+                    explicit api_exception(const std::string & message) noexcept :
+                        std::exception(message.c_str())
+                    {}
 
-                m_state.call(1, 1);
+                    explicit api_exception(const char * const message) noexcept :
+                        std::exception(message)
+                    {}
 
-                return m_state.top < std::string > ().get();
-            }
+                    ~api_exception() noexcept = default;
+                };
 
-        private:
+                class API
+                {
+                public:
 
-            State m_state;
-        };
+                    explicit API(State state) : m_state(state)
+                    {}
 
-    } // namespace lua
+                    ~API() noexcept = default;
 
-} // namespace detail
+                public:
+
+                    const auto state() const noexcept
+                    {
+                        return m_state;
+                    }
+
+                public:
+
+                    auto is_connected() const
+                    {
+                        return m_state.call < unsigned int > ("isConnected");
+                    }
+
+                    auto send_message(const std::string & message) const
+                    {
+                        return m_state.call < unsigned int > ("message", message);
+                    }
+
+                    auto get_portfolio(const std::string & client_id, const std::string & client_code) const
+                    {
+                        return m_state.call < tables::Portfolio > ("getPortfolioInfo", client_id, client_code);
+                    }
+
+                    auto get_security_info(const std::string & class_code, const std::string & asset_code) const
+                    {
+                        return m_state.call < tables::Asset > ("getSecurityInfo", class_code, asset_code);
+                    }
+
+                    auto send_transaction(const std::unordered_map < std::string, std::string > & transaction) const
+                    {
+                        m_state.get_global("sendTransaction");
+
+                        m_state.new_table();
+
+                        for (const auto & [field, value] : transaction)
+                        {
+                            m_state.push_string(field);
+                            m_state.push_string(value);
+
+                            m_state.set_table(-3);
+                        }
+
+                        m_state.call(1, 1);
+
+                        return m_state.top < std::string > ().get();
+                    }
+
+                private:
+
+                    State m_state;
+                };
+
+            } // namespace lua
+
+        } // namespace detail
+
+    } // namespace plugin
+
+} // namespace solution
 
 #endif // #ifndef SOLUTION_PLUGIN_DETAIL_LUA_API_HPP
