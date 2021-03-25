5630649fb93e73e3fce623074e4225a1b82bd853
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-03-25 15:49:11 +0300

    Добавлен класс Source, обеспечивающий чтение данных свечных графиков из разделяемой памяти.

---
+++ projects/system/source/market/source/source.cpp
@@ -0,0 +1,118 @@
+#include "source.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace market
+		{
+			using Severity = shared::Logger::Severity;
+
+			void Source::initialize()
+			{
+				RUN_LOGGER(logger);
+
+				try
+				{
+					initialize_shared_memory();
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < source_exception > (logger, exception);
+				}
+			}
+
+			void Source::uninitialize()
+			{
+				RUN_LOGGER(logger);
+
+				try
+				{
+					m_shared_memory.destroy_ptr(m_deque);
+
+					const auto shared_memory_name = make_shared_memory_name();
+
+					boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str());
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < source_exception > (logger, exception);
+				}
+			}
+
+			void Source::initialize_shared_memory()
+			{
+				RUN_LOGGER(logger);
+
+				try
+				{
+					const auto shared_memory_name = make_shared_memory_name();
+
+					m_shared_memory = shared_memory_t(boost::interprocess::open_only, shared_memory_name.c_str());
+
+					m_deque = m_shared_memory.find < deque_t > (boost::interprocess::unique_instance).first;
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < source_exception > (logger, exception);
+				}
+			}
+
+			std::string Source::make_shared_memory_name() const
+			{
+				RUN_LOGGER(logger);
+
+				try
+				{
+					std::string shared_memory_name = "QUIK_" + m_asset + "_";
+
+					if (m_scale == "M60")
+					{
+						shared_memory_name += "H";
+					}
+					else
+					{
+						shared_memory_name += m_scale;
+					}
+
+					shared_memory_name += "1";
+
+					return shared_memory_name;
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < source_exception > (logger, exception);
+				}
+			}
+
+			std::string Source::get(std::size_t size) const
+			{
+				RUN_LOGGER(logger);
+
+				try
+				{
+					std::string result;
+
+					const auto current_size = m_deque->size();
+
+					if (current_size >= size)
+					{
+						for (auto i = 0U; i < size; ++i)
+						{
+							result += std::string((*m_deque)[current_size - size + i].c_str()) + '\n';
+						}
+					}
+
+					return result;
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < source_exception > (logger, exception);
+				}
+			}
+
+		} // namespace market
+
+	} // namespace system
+
+} // namespace solution
---
+++ projects/system/source/market/source/source.hpp
@@ -0,0 +1,136 @@
+#ifndef SOLUTION_SYSTEM_MARKET_SOURCE_HPP
+#define SOLUTION_SYSTEM_MARKET_SOURCE_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include <exception>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/containers/deque.hpp>
+#include <boost/interprocess/containers/string.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+
+#include "../../../../shared/source/logger/logger.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace market
+		{
+			class source_exception : public std::exception
+			{
+			public:
+
+				explicit source_exception(const std::string & message) noexcept :
+					std::exception(message.c_str())
+				{}
+
+				explicit source_exception(const char * const message) noexcept :
+					std::exception(message)
+				{}
+
+				~source_exception() noexcept = default;
+			};
+
+			class Source
+			{
+			private:
+
+				using shared_memory_t = boost::interprocess::managed_shared_memory;
+
+				using segment_manager_t = shared_memory_t::segment_manager;
+
+				using char_allocator_t = boost::interprocess::allocator < char, segment_manager_t > ;
+
+				using record_t = boost::interprocess::basic_string < char, std::char_traits < char >, char_allocator_t > ;
+
+				using record_allocator_t = boost::interprocess::allocator < record_t, segment_manager_t > ;
+
+				using deque_t = boost::interprocess::deque < record_t, record_allocator_t > ;
+
+			public:
+
+				template < typename A, typename S, typename Enable =
+					std::enable_if_t <
+						std::is_convertible_v < A, std::string > &&
+						std::is_convertible_v < S, std::string > > >
+				explicit Source(A && asset, S && scale) : 
+						m_asset(std::forward < A > (asset)), 
+						m_scale(std::forward < S > (scale))
+				{
+					initialize();
+				}
+
+				~Source() noexcept
+				{
+					try
+					{
+						uninitialize();
+					}
+					catch (...)
+					{
+						// std::abort();
+					}
+				}				
+
+			private:
+
+				void initialize();
+
+				void uninitialize();
+
+			private:
+
+				void initialize_source();
+
+				void initialize_shared_memory();
+
+			private:
+
+				std::string make_shared_memory_name() const;
+
+			public:
+
+				const auto & asset() const noexcept
+				{
+					return m_asset;
+				}
+
+				const auto & scale() const noexcept
+				{
+					return m_scale;
+				}
+
+			public:
+
+				std::string get(std::size_t size) const;
+
+			private:
+
+				const std::string m_asset;
+				const std::string m_scale;
+
+			private:
+
+				shared_memory_t m_shared_memory;
+
+				deque_t * m_deque;
+			};
+
+		} // namespace market
+
+	} // namespace system
+
+} // namespace solution
+
+#endif // #ifndef SOLUTION_SYSTEM_MARKET_SOURCE_HPP
