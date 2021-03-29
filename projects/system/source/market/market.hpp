c81ef2e61408edc5ccf20c62d39a2e8dbcaf7514
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-03-29 18:18:09 +0300

    Убраны лишние упоминания о Python из заголовочного файла класса Market.

--- projects/system/source/market/market.hpp
+++ projects/system/source/market/market.hpp
@@ -32,14 +32,11 @@
 #include <unordered_map>
 #include <vector>
 
-#define BOOST_PYTHON_STATIC_LIB
-
 #include <boost/asio.hpp>
 #include <boost/config/warning_disable.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
 #include <boost/multi_array.hpp>
-#include <boost/python.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
