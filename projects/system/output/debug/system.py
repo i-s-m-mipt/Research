ac780455c2123fbd86a4a099942d5bf1887d81a3
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-04-10 01:13:37 +0300

    Обновлен скрипт для загрузки и парсинга таблицы дивидендов.

--- projects/system/output/debug/system.py
+++ projects/system/output/debug/system.py
@@ -15,14 +15,10 @@ def get_dividends():
             self.inside_table = False
             self.inside_row = False
             self.inside_cell = False
-            self.crutch = ""
             self.data = []
 
         def handle_starttag(self, tag, attrs):
-            if self.inside_cell and tag == 'br':
-                self.crutch = self.data[-1][-1]
-                self.data[-1].pop(-1)
-            if self.inside_row and tag in ['td', 'th']:
+            if self.inside_row and tag == 'td':
                 self.inside_cell = True
             if self.inside_table and tag == 'tr':
                 self.inside_row = True
@@ -32,8 +28,7 @@ def get_dividends():
 
         def handle_data(self, data):
             if self.inside_cell:
-                self.data[-1].append(self.crutch + data.strip())
-                self.crutch = ""
+                self.data[-1].append(data.strip())
 
         def handle_endtag(self, tag):
             if tag == 'table':
@@ -51,12 +46,8 @@ def get_dividends():
     parser.feed(document)
     parsed = parser.data
 
-    parsed[0] = [x for x in parsed[0][::2] if len(x) > 0]
-    parsed[0].insert(3, '')
-    parsed[1] = parsed[1][1:]
-
     parsed = [{'asset': row[1], 'dividend': float(row[4].replace(',', '.')), 'buy_date': row[7], 'gap_date': row[8]}
-              for row in parsed[1:] if len(row) == len(parsed[0])]
+              for row in parsed[1:] if len(row) == len(parsed[1])]
     return json.dumps(parsed)
 
 # load trained models here as globals
