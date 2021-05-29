90ca4463137a908b91fb0da2aa077b523c289ea9
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-05-29 19:37:48 +0300

    Добавлена заготовка программы-автозапуска терминала QUIK.

---
+++ projects/loader/source/main.cpp
@@ -0,0 +1,38 @@
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include "../../shared/source/logger/logger.hpp"
+
+using Logger = solution::shared::Logger;
+
+int main(int argc, char ** argv)
+{
+	RUN_LOGGER(logger);
+
+	try
+	{
+		// TODO
+
+		system("pause");
+
+		return EXIT_SUCCESS;
+	}
+	catch (const std::exception & exception)
+	{
+		logger.write(Logger::Severity::fatal, exception.what());
+
+		system("pause");
+
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		logger.write(Logger::Severity::fatal, "unknown exception");
+
+		system("pause");
+
+		return EXIT_FAILURE;
+	}
+}
