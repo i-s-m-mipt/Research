df2ddb29579c6cf2a17a776a82cdff5ab2b5422d
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-05-30 14:50:03 +0300

    Реализована заготовка программы-загрузчика Quik.

--- projects/loader/source/main.cpp
+++ projects/loader/source/main.cpp
@@ -1,11 +1,49 @@
+#include <ctime>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 
+#include <boost/process.hpp>
+
 #include "../../shared/source/logger/logger.hpp"
 
 using Logger = solution::shared::Logger;
+
+class loader_exception : public std::exception
+{
+public:
+
+	explicit loader_exception(const std::string & message) noexcept :
+		std::exception(message.c_str())
+	{}
+
+	explicit loader_exception(const char * const message) noexcept :
+		std::exception(message)
+	{}
+
+	~loader_exception() noexcept = default;
+};
+
+bool is_session_open()
+{
+	RUN_LOGGER(logger);
+
+	try
+	{
+		auto time = std::time(nullptr);
+
+		auto tm = *std::localtime(&time);
+
+		return (
+			((tm.tm_hour > 9)  || (tm.tm_hour == 9  && tm.tm_min > 50)) &&
+			((tm.tm_hour < 23) || (tm.tm_hour == 23 && tm.tm_min < 59)));
+	}
+	catch (const std::exception & exception)
+	{
+		solution::shared::catch_handler < loader_exception > (logger, exception);
+	}
+}
 
 int main(int argc, char ** argv)
 {
@@ -13,9 +51,19 @@ int main(int argc, char ** argv)
 
 	try
 	{
-		// TODO
+		while (!is_session_open())
+		{
+			std::this_thread::yield();
+		}
 
-		system("pause");
+		boost::process::child quik("info.exe");
+
+		while (is_session_open())
+		{
+			std::this_thread::yield();
+		}
+
+		//system("pause");
 
 		return EXIT_SUCCESS;
 	}
