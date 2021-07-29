52eaa851467307fe2325d12739b9857be09c1314
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-07-29 18:10:37 +0300

    Добавлен индикатор VWMA.

---
+++ projects/system/source/market/indicators/vwma/vwma.cpp
@@ -0,0 +1,64 @@
+#include "vwma.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace market
+		{
+			namespace indicators
+			{
+				void VWMA::initialize() const
+				{
+					RUN_LOGGER(logger);
+
+					try
+					{
+						if (m_timesteps == 0U)
+						{
+							throw std::domain_error("invalid timesteps value");
+						}
+					}
+					catch (const std::exception & exception)
+					{
+						shared::catch_handler < indicator_exception > (logger, exception);
+					}
+				}
+
+				void VWMA::operator()(candles_container_t & candles) const
+				{
+					RUN_LOGGER(logger);
+
+					try
+					{
+						for (auto i = m_timesteps - 1U; i < std::size(candles); ++i)
+						{
+							auto value = 0.0;
+
+							market::Candle::volume_t total_volume = 0ULL;
+
+							for (auto j = i + 1U - m_timesteps; j <= i; ++j)
+							{
+								value += candles[j].price_close * candles[j].volume;
+
+								total_volume += candles[j].volume;
+							}
+
+							value /= total_volume;
+
+							candles[i].indicators.push_back(value);
+						}
+					}
+					catch (const std::exception & exception)
+					{
+						shared::catch_handler < indicator_exception > (logger, exception);
+					}
+				}
+
+			} // namespace indicators
+
+		} // namespace market
+
+	} // namespace system
+
+} // namespace solution
