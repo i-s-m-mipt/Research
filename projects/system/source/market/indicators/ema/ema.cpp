75ee71e59e4b94fdbbf867c4107787470cbae22e
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-07-29 18:11:35 +0300

    Добавлен индикатор EMA.

---
+++ projects/system/source/market/indicators/ema/ema.cpp
@@ -0,0 +1,56 @@
+#include "ema.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace market
+		{
+			namespace indicators
+			{
+				void EMA::initialize() const
+				{
+					RUN_LOGGER(logger);
+
+					try
+					{
+						if (m_timesteps == 0U)
+						{
+							throw std::domain_error("invalid timesteps value");
+						}
+					}
+					catch (const std::exception & exception)
+					{
+						shared::catch_handler < indicator_exception > (logger, exception);
+					}
+				}
+
+				void EMA::operator()(candles_container_t & candles) const
+				{
+					RUN_LOGGER(logger);
+
+					try
+					{
+						candles.front().indicators.push_back(candles.front().price_close);
+
+						auto k = 2.0 / (m_timesteps + 1.0);
+
+						for (auto i = 1U; i < std::size(candles); ++i)
+						{
+							candles[i].indicators.push_back(k * candles[i].price_close +
+								(1.0 - k) * candles[i - 1U].indicators.back());
+						}
+					}
+					catch (const std::exception & exception)
+					{
+						shared::catch_handler < indicator_exception > (logger, exception);
+					}
+				}
+
+			} // namespace indicators
+
+		} // namespace market
+
+	} // namespace system
+
+} // namespace solution
