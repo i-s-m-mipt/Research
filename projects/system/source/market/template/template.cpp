ced561016637d0ed7fcaa880116227a85baaaccd
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-07-29 18:02:39 +0300

    Добавлен класс-шаблон для реализации индикаторов техничесского анализа.

---
+++ projects/system/source/market/template/template.cpp
@@ -0,0 +1,48 @@
+#include "template.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace market
+		{
+			namespace indicators
+			{
+				void TEMPLATE::initialize() const
+				{
+					RUN_LOGGER(logger);
+
+					try
+					{
+						if (m_timesteps == 0U)
+						{
+							throw std::domain_error("invalid timesteps value");
+						}
+					}
+					catch (const std::exception & exception)
+					{
+						shared::catch_handler < indicator_exception > (logger, exception);
+					}
+				}
+
+				void TEMPLATE::operator()(candles_container_t & candles) const
+				{
+					RUN_LOGGER(logger);
+
+					try
+					{
+						// ...
+					}
+					catch (const std::exception & exception)
+					{
+						shared::catch_handler < indicator_exception > (logger, exception);
+					}
+				}
+
+			} // namespace indicators
+
+		} // namespace market
+
+	} // namespace system
+
+} // namespace solution
