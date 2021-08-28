853ed5ecfb9524e0b5ed9c4b066beac2b23efdc2
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-08-28 20:54:05 +0300

    Осциллятор AROON разделен на 2 осциллятора.

--- projects/system/source/market/oscillators/aroon/aroon.cpp
+++ projects/system/source/market/oscillators/aroon/aroon.cpp
@@ -31,13 +31,19 @@ namespace solution
 
 					try
 					{
-						for (auto candle = std::next(std::begin(candles), m_timesteps); candle != std::end(candles); ++candle)
+						for (auto candle = std::next(std::begin(candles), m_timesteps); 
+								candle != std::end(candles); ++candle)
 						{
-							auto min_max = std::minmax_element(std::prev(candle, m_timesteps), std::next(candle),
-								[](const auto & lhs, const auto & rhs) { return (lhs.price_close < rhs.price_close); });
+							auto min_max_candle = std::minmax_element(
+								std::prev(candle, m_timesteps), std::next(candle),
+									[](const auto & lhs, const auto & rhs)
+										{ return (lhs.price_close < rhs.price_close); });
 
-							candle->oscillators.push_back((std::distance(min_max.first, candle) - 
-								std::distance(min_max.second, candle)) * 100.0 / m_timesteps);
+							candle->oscillators.push_back(100.0 * (m_timesteps - 
+								std::distance(min_max_candle.first, candle)) / m_timesteps);
+
+							candle->oscillators.push_back(100.0 * (m_timesteps -
+								std::distance(min_max_candle.second, candle)) / m_timesteps);
 						}
 					}
 					catch (const std::exception & exception)
